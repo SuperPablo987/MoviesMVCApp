1679740d613410d6533734f55fcb4653ded0faa6
Peter Thiel <thiel@ualberta.ca>
2023-04-17 14:43:21 -0600

    Built Class Library, Migration and Manager for Movies Database

---
+++ MoviesData/MovieManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesData
+{
+    /// <summary>
+    /// methods for working with Movie table in Movie database
+    /// </summary>
+    public static class MovieManager
+    {
+        /// <summary>
+        /// retrieves list of movies from the database
+        /// </summary>
+        /// <returns>list of movies or null if none</returns>
+        public static List<Movie> GetMovies()
+        {
+            List<Movie> movies = null;
+            using (MovieContext dB = new MovieContext()) 
+            {
+                movies = dB.Movies.ToList();
+            }
+
+            return movies;
+        }
+    }
+}
